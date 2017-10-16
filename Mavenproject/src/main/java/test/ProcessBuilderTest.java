dc2e62eda573052ebd5c16afa5ace052f3560a04
Daniel <axhammar.daniel@gmail.com>
2017-10-16 11:36:30 +0200

    test again

--- Mavenproject/src/main/java/test/ProcessBuilderTest.java
+++ Mavenproject/src/main/java/test/ProcessBuilderTest.java
@@ -1,23 +1,23 @@
-package test;
-
-import java.io.IOException;
-
-
-public class ProcessBuilderTest {
-
-    public static void main(String arg[]) throws IOException {
-
-        ProcessBuilder pb = new ProcessBuilder(
-                "curl",
-                "-s",
-                "-XPOST 'http://admin:admin@localhost:8080/createItem?name=CopiedJob' ",
-                "--data-binary @mylocaltestconfig.xml ",
-                "-H",
-                "\"Content-Type:text/xml\""
-                );
-
-        pb.start();
-
-
-    }
-}
+//package test;
+//
+//import java.io.IOException;
+//
+//
+//public class ProcessBuilderTest {
+//
+//    public static void main(String arg[]) throws IOException {
+//
+//        ProcessBuilder pb = new ProcessBuilder(
+//                "curl",
+//                "-s",
+//                "-XPOST 'http://admin:admin@localhost:8080/createItem?name=CopiedJob' ",
+//                "--data-binary @mylocaltestconfig.xml ",
+//                "-H",
+//                "\"Content-Type:text/xml\""
+//                );
+//
+//        pb.start();
+//
+//
+//    }
+//}
