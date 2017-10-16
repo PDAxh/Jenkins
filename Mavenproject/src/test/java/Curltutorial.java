9b7fb0463985920c8b1b1bf98a0d75fa596fdc4d
Daniel <axhammar.daniel@gmail.com>
2017-10-16 11:09:31 +0200

    add dependencies

--- Mavenproject/src/test/java/Curltutorial.java
+++ Mavenproject/src/test/java/Curltutorial.java
@@ -1,6 +1,6 @@
-public class Curltutorial {
-
-    public static void main(String[] args) {
-
-    }
-}
+//public class Curltutorial {
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
